ce59271627bb089ccd5c0466d5d8e6aef499b0dc
buchanam <buchanam@oregonstate.edu>
2020-08-17 15:35:28 -0700

    worked on webpage article

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-Personal React webpage hosted on Github Page: [michaelabuchanan.github.io](michaelabuchanan.github.io)
+Personal React webpage hosted on Github Page: [michaelabuchanan.github.io](michaelabuchanan.github.io)\n
 See site for post on how to set up a website like this.
 
 To-Do:
-- [ ] Set up webpage templates and routing
+- [x] Set up webpage templates and routing
 - [ ] Write blogs for GANs and website setup
 - [ ] Home page interactive terminal style theme (blinking cursor, bottom info window)
--- src/pages/Gitpages.js
+++ src/pages/Gitpages.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 import Paper from '@material-ui/core/Paper';
 
+import react_logo from "../react_logo.jpg"
+
 import './Article.css';
 
 function Gitpages() {
@@ -10,9 +12,21 @@ function Gitpages() {
       <header className="App-header">
         <p className="breadcrumb"><code><NavLink exact activeClassName="active" style={{color: 'cyan'}} to="/">Articles</NavLink> &gt; Git-Pages</code></p>
 
-        <p>How to host your React app on Git-Pages for free </p>
+        <p><strong>How to host your React app on Git-Pages for free </strong></p>
+        <img src={react_logo} alt="react logo" height="350" width="500"/>
+        <p className="contents"><code><span className="red"><a href="#overview">Overview</a></span> -&gt; <span className="yellow">Prerequisites</span> -&gt; <span className="green">Setup React Project</span> -&gt; <span className="green">Setup GitHub Repo</span> -&gt; <span className="red">Comments</span></code></p>
 
-        <Paper elevation={3} />
+        <p id="overview" className="subject">Overview</p>
+        <p class="text">Are you looking for a free hosting service for your React application? If the application is for personal or development use, then GitPages may be a great alternative to more traditional website hosting services. </p>
+
+        <p id="overview" className="subject">Prerequisites</p>
+
+        <p id="overview" className="subject">Setting Up Your React Project</p>
+
+        <p id="overview" className="subject">Setting Up Your Github Repository</p>
+
+        <p id="overview" className="subject">Comments</p>
+
       </header>
     </div>
   );
