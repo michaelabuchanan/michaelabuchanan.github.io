c930d923547fdebeec4f59ce22157bb6be0cf00b
buchanam <buchanam@oregonstate.edu>
2020-08-17 08:41:58 -0700

    Updates

---
+++ precache-manifest.84fb5618dd9049021dd56688354010cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca27a23522353c14d1287f76e2595f23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9b3a8c812e0d6ebfe04",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b6da0c7b1b752d978fec",
+    "url": "/static/js/2.44bd9126.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44bd9126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b3a8c812e0d6ebfe04",
+    "url": "/static/js/main.8487a1a3.chunk.js"
+  },
+  {
+    "revision": "ceba7229b1d77e17a7f6",
+    "url": "/static/js/runtime-main.aa46124a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
