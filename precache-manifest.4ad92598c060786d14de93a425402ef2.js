ce228146daf6bd06263d4c471647766169c5e66d
buchanam <buchanam@oregonstate.edu>
2020-10-29 09:40:56 -0700

    Updates

---
+++ precache-manifest.4ad92598c060786d14de93a425402ef2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "574d3e7f5022a1896d7865f030c6279b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f7b8dc7ff6bcf8bc436",
+    "url": "/static/css/main.21016717.chunk.css"
+  },
+  {
+    "revision": "fb484556459548e36eb5",
+    "url": "/static/js/2.50473989.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.50473989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7b8dc7ff6bcf8bc436",
+    "url": "/static/js/main.b2d89daf.chunk.js"
+  },
+  {
+    "revision": "ceba7229b1d77e17a7f6",
+    "url": "/static/js/runtime-main.aa46124a.js"
+  },
+  {
+    "revision": "6bc3283f3c5b2239af3845af0ba457a1",
+    "url": "/static/media/attempt_2_final.6bc3283f.png"
+  },
+  {
+    "revision": "68bb1f064dd2c58de39fe3cdcf9b9c13",
+    "url": "/static/media/dsm.68bb1f06.jpg"
+  },
+  {
+    "revision": "4621a2870968cf8912c3818591df6e0a",
+    "url": "/static/media/fakes010296.4621a287.png"
+  },
+  {
+    "revision": "6d73352f5a8ff851e84409a8fc981e42",
+    "url": "/static/media/fakes012000-2.6d73352f.png"
+  },
+  {
+    "revision": "7fa0522009b9bc590fa492d839022a27",
+    "url": "/static/media/fakes012000.7fa05220.png"
+  },
+  {
+    "revision": "be1cab51fad1daf3092b6ef4071bcb3a",
+    "url": "/static/media/fakes012000.be1cab51.png"
+  },
+  {
+    "revision": "43f253c1f81a1530e8c04aea6415f554",
+    "url": "/static/media/git_sh.43f253c1.png"
+  },
+  {
+    "revision": "7bdceb1544f6f4a90c15fd3e2656f815",
+    "url": "/static/media/react_logo.7bdceb15.jpg"
+  },
+  {
+    "revision": "77d8ff06a480add000f157577fcc4495",
+    "url": "/static/media/starion.77d8ff06.jpg"
+  }
+]);
