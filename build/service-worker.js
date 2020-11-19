04d83854c30461a8da8f892e9708807e4199157b
buchanam <buchanam@oregonstate.edu>
2020-11-19 13:28:59 -0800

    more font size changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78a0a39c2975206daf2167acc6248515.js"
+  "/precache-manifest.4452dd212d8095254f5464211dbbda9e.js"
 );
 
 self.addEventListener('message', (event) => {
