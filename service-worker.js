92bc0a1e4dfbd73dd89721484c097c4646f7b7e5
buchanam <buchanam@oregonstate.edu>
2020-11-25 10:56:02 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.221217770851e482d409d0dfec412050.js"
+  "/precache-manifest.e2f175e4279ac13f9d15b419819a279c.js"
 );
 
 self.addEventListener('message', (event) => {
